12d283b23389228243af58c47b2a94db2e8fe554
Prasit5 <pdol1@my.centennialcollege.ca>
2023-09-15 15:49:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const connect = require('connect');
+const app = connect();
+
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+ };
+
+function helloWorld(req, res, next) {
+   res.setHeader('Content-Type', 'text/plain');
+   res.end('Hello World');
+};
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Good bye World');
+ };
+  
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye',goodbyeWorld);
+app.listen(9090);
+console.log('Server running at http://localhost:3000/');
